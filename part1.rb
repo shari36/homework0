b3f95644b9c64862ca52387fd5908c0af449c696
Shari <shevi36@gmail.com>
2014-09-29 16:29:36 -0300

    part1 commit

--- part1.rb
+++ part1.rb
@@ -1,10 +1,28 @@
-def sum(array_of_ints)
+def sum(int_array)
   total = 0
-  array_of_ints.each do |num|
+  int_array.each do |num|
      total += num
   end
   return total
 end
 
-my_array = [1, 2, 3, 4]
-puts sum(my_array)
+def max_2_sum(int_array)
+  int_array.sort
+  return int_array.last + int_array[int_array.length-2] 
+end
+
+def sum_to_n?(int_array, n)
+  int_array.each do |i|
+    int_array.each do |j|
+      unless i == j  
+        if(i+j) == n
+          return true
+        end
+      end
+    end
+  end
+  return false
+end
+
+
+
